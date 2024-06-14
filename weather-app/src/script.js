145dbd1aad599f587b2bb6f03662dd2a13a74758
Rizwan Fayaz <168568097+rizwanfayaz@users.noreply.github.com>
2024-06-14 21:57:23 +0530

    Update script.js

--- weather-app/src/script.js
+++ weather-app/src/script.js
@@ -1,4 +1,4 @@
-const apiKey = "";  //get api key from readme.md
+const apiKey = "";  //get api key from readme.md ---- removed for security reasons.
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
